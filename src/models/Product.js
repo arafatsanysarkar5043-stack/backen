74feb6caf37d6183d9353f64d3556c17f2f14536
arafatsanysarkar5043-stack <arafatsanysarkar5043@gmail.com>
2026-09-13 17:13:23 +0600

    Update Product.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -40,6 +40,6 @@ const schema = new mongoose.Schema({
   active: { type: Boolean, default: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
-}, { versionKey: true });
+}, { versionKey: false });
 schema.add({ security: { type: securitySchema, required: true } });
 export default mongoose.model('Product', schema);
